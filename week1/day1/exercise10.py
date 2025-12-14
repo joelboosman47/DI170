8193d0f3d0bfc5f8857ce6f4be9afe3f366a94c1
Joël Boosman <joel_boosman@hotmail.com>
2025-12-14 22:16:24 +0200

    Finalised last exercise

--- week1/day1/exercise10.py
+++ week1/day1/exercise10.py
@@ -1,14 +1,11 @@
 # Ask the user for a number between 1 and 100
-
-# If the number is a divisible by three, print Fizz
-
-# If the number is a divisible by five, print Buzz.
-
-# If the number is a divisible by both three and five, print FizzBuzz instead.
-
 number = int(input("Give me a number between 1 and 100: "))
 
-while number < 1 or number > 100:
+# If the number is a divisible by three, print Fizz
+# If the number is a divisible by five, print Buzz.
+# If the number is a divisible by both three and five, print FizzBuzz instead.
+
+while number < 1 or number > 100: #this was my own addition, to make it more fool-proof ;)
     print("""You didn't follow my instructions!
           Try again""")
     number = int(input("Give me a number between 1 and 100"))
@@ -19,5 +16,7 @@ elif number % 3 == 0:
     print("Fizz")
 elif number % 5 == 0:
     print("Buzz")
-else: 
+else: # not requested, but I like to have an else statement so that you see some output if all conditions are false.
     print("Ok")
+
+
