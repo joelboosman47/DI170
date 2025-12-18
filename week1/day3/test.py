2332ffe021970c59a7597a98bc79d117aa98b026
Joël Boosman <joel_boosman@hotmail.com>
2025-12-18 13:06:49 +0200

    test script from day3

---
+++ week1/day3/test.py
@@ -0,0 +1,24 @@
+# # my_list = [1,2,3,4,6,5]
+
+# # print(sorted(my_list))
+
+# # my_list.sort()
+
+# # print(my_list)
+
+# list1 = [5, 10, 15, 20, 25, 50, 20]
+# index_of_item = list1.index(20)
+
+# list1[index_of_item] = 200
+# print(list1)
+
+# number_given = int(input('Enter a number: '))
+
+# for i in range(10):
+#     print(i * number_given)
+
+i = 1
+
+while i < 11:
+    print(i)
+    i += 1
