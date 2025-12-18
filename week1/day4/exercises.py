508060ed49f03af2fa79819260131418fa48e3fd
Joël Boosman <joel_boosman@hotmail.com>
2025-12-19 01:20:35 +0200

    exercises day4

--- week1/day4/exercises.py
+++ week1/day4/exercises.py
@@ -145,3 +145,67 @@ if favorite_fruit.__contains__(user_input2): #check if newly entered name appear
 else:
     print("You chose a new fruit. I hope you enjoy it!")
 
+# 🌟 Exercise 8: Pizza Toppings
+# Key Python Topics:
+
+# Loops
+# Lists
+# String formatting
+
+# Instructions:
+
+# Write a loop that asks the user to enter pizza toppings one by one.
+# Stop the loop when the user types 'quit'.
+# For each topping entered, print:
+# "Adding [topping] to your pizza."
+# After exiting the loop, print all the toppings and the total cost of the pizza.
+# The base price is $10, and each topping adds $2.50.
+price = 10.0 #basic price of pizza (in float, since we will add a float for each topping)
+chosen_toppings = []
+user_input = input("Enter a pizza topping: ")
+chosen_toppings.append(user_input) #add the input to the list
+price += 2.50 #add 2.50 to the price float
+while True: #keep asking for input. If the user enters 'quit', stop the loop.
+    user_input = input("Enter another pizza topping (enter 'quit' to stop): ")
+    if user_input == 'quit': break
+    chosen_toppings.append(user_input) #add the input to the list
+    price += 2.50 #add 2.50 to the price like before
+#after the loop has ended, print the list of inputs and the price value (on a next line)
+print(f'''Your chosen toppings are: {chosen_toppings}. 
+Your pizza costs: {price}.''')
+
+# 🌟 Exercise 9: Cinemax Tickets
+# Key Python Topics:
+
+# Conditionals
+# Lists
+# Loops
+
+# Instructions:
+
+# Ask for the age of each person in a family who wants to buy a movie ticket.
+# Calculate the total cost based on the following rules:
+# Free for people under 3.
+# $10 for people aged 3 to 12.
+# $15 for anyone over 12.
+# Print the total ticket cost.
+cost_tickets = 0
+family_ages = input("Please enter the ages of each person who wants to buy a ticket (comma separated): ").split(',')
+family_ages = [int(age) for age in family_ages]
+for age in family_ages:
+    if age < 3: continue
+    if 3 <= age <= 12:
+        cost_tickets += 10
+    else:
+        cost_tickets += 15
+print(f"Your tickets cost total: ${cost_tickets}.") 
+# Bonus:
+
+# Imagine a group of teenagers wants to see a restricted movie (only for ages 16–21).
+# Write a program to:
+# Ask for each person’s age.
+# Remove anyone who isn’t allowed to watch.
+# Print the final list of attendees.
+teenager_ages = input("Please enter your ages (comma separated): ").split(',')
+teenager_ages = [int(age) for age in teenager_ages if 16 <= int(age) <= 21]
+print(f"Final list of attendees: {teenager_ages}")
