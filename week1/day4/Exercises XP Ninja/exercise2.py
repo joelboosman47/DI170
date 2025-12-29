2399fcd82e2024a45eb0d69de348c448e9b6facb
Joël Boosman <joel_boosman@hotmail.com>
2025-12-29 19:00:55 +0200

    exercise 2

--- week1/day4/Exercises XP Ninja/exercise2.py
+++ week1/day4/Exercises XP Ninja/exercise2.py
@@ -2,41 +2,53 @@
 # Instructions
 # Given a list of 10 integers to analyze. For example:
 
-#     [3, 47, 99, -80, 22, 97, 54, -23, 5, 7] 
+list1 = [3, 47, 99, -80, 22, 97, 54, -23, 5, 7] 
 #     [44, 91, 8, 24, -6, 0, 56, 8, 100, 2] 
 #     [3, 21, 76, 53, 9, -82, -3, 49, 1, 76] 
 #     [18, 19, 2, 56, 33, 17, 41, -63, -82, 1]
+import math
 
 # 1. Store the list of numbers in a variable.
 
 # 2. Print the following information:
 # a. The list of numbers – printed in a single line
+a = ' '.join(str(x) for x in list1) 
+print(a)
 # b. The list of numbers – sorted in descending order (largest to smallest)
+list1.sort(reverse=True)
+print("Sorted desc:", list1)
 # c. The sum of all the numbers
+sum_of_list1 = sum(list1)
+print("Sum:", sum_of_list1)
 
 # 3. A list containing the first and the last numbers.
+list2 = [list1[0], list1[-1]]
+print (list2)
 
 # 4. A list of all the numbers greater than 50.
+list3 = [i for i in list1 if i > 50]
+print(list3)
 
 # 5. A list of all the numbers smaller than 10.
+list4 = [i for i in list1 if i < 10]
+print(list4)
 
 # 6. A list of all the numbers squared – eg. for [1, 2, 3] you would print “1 4 9”.
+list5 = [i**2 for i in list1]
+print(list5)
 
 # 7. The numbers without any duplicates – also print how many numbers are in the new list.
+list1.extend([3, 5, 7])
+list6 = list(set(list1))
+print(list6)
+print("len: ", len(list6))
 
 # 8. The average of all the numbers.
+average = math.floor(sum(list1) / len(list1))
+print("Average:", average)
 
 # 9. The largest number.
+print('Max:', max(list1))
 
 # 10.The smallest number.
-
-# 11. Bonus: Find the sum, average, largest and smallest number without using built in functions.
-
-# 12. Bonus: Instead of using pre-defined lists of numbers, ask the user for 10 numbers between -100 and 100. Ask the user for an integer between -100 and 100 – repeat this question 10 times. Each number should be added into a variable that you created earlier.
-
-# 13. Bonus: Instead of asking the user for 10 integers, generate 10 random integers yourself. Make sure that these random integers are between -100 and 100.
-
-# 14. Bonus: Instead of always generating 10 integers, let the amount of integers also be random! Generate a random positive integer no smaller than 50.
-
-# 15. Bonus: Will the code work when the number of random numbers is not equal to 10?
-
+print('Min:', min(list1))
