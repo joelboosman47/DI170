c7ebed1b00fe0c9ddfe846664c74e67bb70d379b
Joël Boosman <joel_boosman@hotmail.com>
2025-12-29 23:01:50 +0200

    add exercise 3: initial commit for paragraph analysis

---
+++ week1/day4/Exercises XP Ninja/exercise3.py
@@ -0,0 +1,8 @@
+# Exercise 3: Working on a paragraph
+# Find an interesting paragraph of text online. (Please keep it appropriate to the social context of our class.)
+# Paste it to your code, and store it in a variable.
+# Let’s analyze the paragraph. Print out a nicely formatted message saying:
+# How many characters it contains (this one is easy…).
+# How many sentences it contains.
+# How many words it contains.
+# How many unique words it contains.
