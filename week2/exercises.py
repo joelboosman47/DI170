1de39db836cbfce3ead8bef2915ce3960cfcaf9b
Joël Boosman <joel_boosman@hotmail.com>
2026-01-04 22:35:46 +0200

    added exercise 3

--- week2/exercises.py
+++ week2/exercises.py
@@ -122,3 +122,39 @@ sarahs_dog.jump()
 #4. Compare Dog Sizes
 print(f'{davids_dog.name} has a height of {davids_dog.height} cm')
 print(f'{sarahs_dog.name} has a height of {sarahs_dog.height} cm')
+
+# 🌟 Exercise 3 : Who’s the song producer?
+# Goal: Create a Song class to represent song lyrics and print them.
+
+# Key Python Topics:
+# Classes and objects
+# Object instantiation
+# Methods
+# Lists
+
+# Instructions:
+# Create a Song class with a method to print song lyrics line by line.
+
+# Step 1: Create the Song Class
+# Create a class called Song.
+# In the __init__ method, take lyrics (a list) as a parameter and create a corresponding attribute.
+# Create a sing_me_a_song() method that prints each element of the lyrics list on a new line.
+
+# Example:
+# stairway = Song(["There’s a lady who's sure", "all that glitters is gold", "and she’s buying a stairway to heaven"])
+# stairway.sing_me_a_song()
+# Output: There’s a lady who’s sureall that glitters is goldand she’s buying a stairway to heaven
+
+#1. create the Song class
+class Song:
+   #In the __init__ method, take lyrics (a list) as a parameter and create a corresponding attribute.
+    def __init__(self, lyrics): 
+        self.lyrics = lyrics
+    
+    #Create a sing_me_a_song() method that prints each element of the lyrics list on a new line.
+    def sing_me_a_song(self):
+       for line in self.lyrics:
+           print(line)
+
+stairway = Song(["There’s a lady who's sure", "all that glitters is gold", "and she’s buying a stairway to heaven"])
+stairway.sing_me_a_song()
