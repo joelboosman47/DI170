74e53154d3910ee248daa57e0b4205cc4bfb5c53
Joël Boosman <joel_boosman@hotmail.com>
2026-01-07 22:38:20 +0200

    exercises day6

---
+++ week1/day6/listcompr.py
@@ -0,0 +1,11 @@
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = set([value for value in some_list if some_list.count(value) > 1])
+
+
+# for value in some_list:
+#     if some_list.count(value) > 1:
+#         if value not in duplicates:
+#             duplicates.append(value)
+
+print(duplicates)
